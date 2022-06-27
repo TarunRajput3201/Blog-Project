5655b4e10a2f50a9d4a7fad207ad9da4c9930b86
TarunRajput3201 <tarunrajput3201@gmail.com>
2022-06-27 11:03:37 +0530

    Tarun

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://TarunRajput3201:B07QeYy2JnV33rgz@cluster0.7eei6gq.mongodb.net/Blog-Project-Final", {
+mongoose.connect("mongodb+srv://TarunRajput3201:B07QeYy2JnV33rgz@cluster0.7eei6gq.mongodb.net/group16Database", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
